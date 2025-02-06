e24b3550fbcf85f3645dc259aab65e9e3175a028
Rachid Echafai <rachidchaf2001@gmail.com>
2025-02-06 11:52:10 +0100

    Add initial project setup with configuration files, DTOs, and basic endpoints

---
+++ src/app.module.ts
@@ -0,0 +1,35 @@
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import databaseConfig from './config/database.config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ErrorHandlerMiddleware } from './common/middlewares/error-handler.middleware';
+import { PharmacyReviewModule } from './pharmacy-review/pharmacy-review.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PharmaciesModule } from './pharmacies/pharmacies.module';
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig],
+      cache: true,
+    }),
+    DatabaseModule,
+    PharmacyReviewModule,
+    AuthModule,
+    UsersModule,
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    PharmaciesModule,
+  ],
+
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(ErrorHandlerMiddleware).forRoutes('*');
+  }
+}
